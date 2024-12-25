0c0e8be87fd39e95194b42121875028502d0b6fc
xamidullo <khkhamidullo@gmail.com>
2024-12-25 15:44:45 +0500

    update app

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const express = require("express");
 const { webhookCallback } = require("grammy");
 const bot = require("./bot.js");
 
-
 const app = express(); // or whatever you're using
 app.use(express.json()); // parse the JSON request body
 
 // "express" is also used as default if no argument is given.
 app.use(webhookCallback(bot, "express"));
 
+app.use((req, res) => {
+  res.send("ok");
+});
 
-app.listen(4444, ()=>
-{
-  console.log("Server running on port 4444")
-})
+app.listen(4444, () => {
+  console.log("Server running on port 4444");
+});
